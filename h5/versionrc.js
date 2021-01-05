13057163def9d33739c08f07e6a3ce67ec7d40ac
wenyujie <wenyujie@hundun.cn>
2021-01-05 14:51:20 +0800

    chore: build

---
+++ h5/versionrc.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: wenyujie
+ * @LastEditors: wenyujie
+ * @Date: 2021-01-05 14:48:46
+ * @LastEditTime: 2021-01-05 14:50:07
+ * @Description: file content
+ * @FilePath: /h5/versionrc.js
+ * @powerd by hundun
+ */
+module.exports = {
+}
